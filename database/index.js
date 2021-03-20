018d9c83b178f491d44ad0a2e76039ff23d04287
Jim Burch <jrburch1@gmail.com>
2021-03-20 14:03:15 -0700

    stubbed out the other 2 queries

--- database/index.js
+++ database/index.js
@@ -15,23 +15,23 @@ client.connect() // returns a promise
 // QUERY FUNCTIONS HERE >>>
 
 // Get a specific product
-const getOneProduct = () => {
-  console.log('One product from the DB!');
+const getOneProduct = (id, callback) => {
+  client.query(`SELECT * FROM products WHERE id = ${id}`, (err, results) => {
+    err ? callback(err) : callback(null, results.rows);
+  })
 };
 
 // Get styles for a product ID
-const getStyles = () => {
-  console.log('Styles from the DB!');
+const getStyles = (id, callback) => {
+  client.query(`SELECT * FROM styles WHERE productId = ${id}`, (err, results) => {
+    err ? callback(err) : callback(null, results.rows);
+  })
 };
 
 // Get related product IDs for a product ID
 const getRelatedProducts = (id, callback) => {
   client.query(`SELECT related_product_id FROM related WHERE current_product_id = ${id}`, (err, results) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, results.rows);
-    }
+    err ? callback(err) : callback(null, results.rows);
   })
 };
 
--- server/index.js
+++ server/index.js
@@ -10,42 +10,29 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
-// Hello World
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-  console.log('Hello World!');
-});
-
 app.listen(PORT, () => {
   console.log(`Listening on localhost:${PORT}`);
 });
 
 // ROUTERS
 
-// Get all products (takes in count in params)
-app.get('/products', (req, res) => {
-  res.sendStatus(200);
-  console.log('Get products!');
-});
-
 // Get specific product
 app.get('/products/:product_id', (req, res) => {
-  db.getOneProduct((err, data) => {
-    err ? (console.log(err), res.sendStatus(500)) : res.sendStatus(200);
+  db.getOneProduct(req.params.product_id, (err, data) => {
+    err ? (console.log(err), res.sendStatus(500)) : res.send(data);
   });
 });
 
 // Get all styles for a specific product
 app.get('/products/:product_id/styles', (req, res) => {
-  db.getStyles((err, data) => {
-    err ? (console.log(err), res.sendStatus(500)) : res.sendStatus(200);
+  db.getStyles(req.params.product_id, (err, data) => {
+    err ? (console.log(err), res.sendStatus(500)) : res.send(data);
   });
 });
 
 // Get all related products of a specific product
 app.get('/products/:product_id/related', (req, res) => {
   db.getRelatedProducts(req.params.product_id, (err, data) => {
-    // err ? (console.log(err), res.sendStatus(500)) : res.send(data);
     if (err) {
       console.log(err);
       res.sendStatus(500);
